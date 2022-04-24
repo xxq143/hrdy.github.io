d891a267e8c4ad19c4d567a043c870dc1997d692
xiaoxianqi <190712289@qq.com>
2022-04-25 00:10:15 +0800

    修改

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:xxq143/hrdy.github.io.git master
+git push -f git@github.com:xxq143/xxq143.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com/xxq143/mynote.git master:gh-pages
