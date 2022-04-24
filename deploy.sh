cb8a1f5b85b63e1b09ea372c7aa64a8fc2d96acc
xiaoxianqi <190712289@qq.com>
2022-04-24 23:46:41 +0800

    修改

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com/xxq143/mynote.git master:gh-pages
+git push -f git@github.com/xxq143/mynote.git main:gh-pages
 
 cd -
